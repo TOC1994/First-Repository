ec9f8b148b560dda126772f9ea6153013b734ad6
Jack Reid <jackdreid@hotmail.co.uk>
2015-03-31 16:13:50 +0100

    Code I'm writing while getting used to python

---
+++ Learning_python.py
@@ -0,0 +1,47 @@
+# -*- coding: iso-8859-1 -*-
+integer = 56
+longinteger=2223337777887654433078
+icecream = 977.235
+string = "Wazzaaaaaap"
+boolean = False
+snapback = True
+
+print type(integer)
+print type(longinteger)
+print type(icecream)
+print type(string)
+print type(boolean)
+
+print boolean and snapback 
+print boolean or snapback
+print not boolean
+
+int1 = 12
+int2 = 69
+float1 = 4.34567
+float2 = 99.90
+
+print int2/int1
+print float(int2) / float(int1)
+print int (float1) 
+print int(snapback)
+
+-- Tuples, lists and dictionaries --
+
+inch = "My name is jack"
+tuple1 = tuple(inch)
+print tuple1
+
+tuple2 = ('Jack', '21', 'Dundee', 'Student')
+print tuple2[0]
+
+list1 = ['Jack', '21', 'Dundee', 'Student']
+print type (list1)
+
+soccermom = [1,2,3,4,5,6,7,8,9,10]
+print soccermom[0:6]
+print soccermom[:5]
+print soccermom [:-3]
+print soccermom [-3:]
+
+print len(soccermom)
